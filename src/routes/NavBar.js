ac02af36c5fdbc609d623ba41105ba76ec2d70df
Rushank Savant <rushanksavant34@gmail.com>
2022-05-06 16:22:40 +0530

    new

--- src/routes/NavBar.js
+++ src/routes/NavBar.js
@@ -13,9 +13,9 @@ export default function NavBar() {
                 <Navbar.Brand href="#home" className="text-danger">NFT Marketplace</Navbar.Brand>
                 <Nav className="mt-2 p-0">
                     <Nav.Link href="/" className="text-danger">Home</Nav.Link>
-                    <Nav.Link href="/create-item" className="text-danger">Create Item</Nav.Link>
+                    <Nav.Link href="/create-item" className="text-danger">Create NFT</Nav.Link>
                     <Nav.Link href="/my-nft" className="text-danger">My NFTs</Nav.Link>
-                    <Nav.Link href="/creator-dashboard" className="text-danger">Creator Dashboard</Nav.Link>
+                    <Nav.Link href="/creator-dashboard" className="text-danger">My Creation</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
