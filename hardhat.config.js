34cd90ef6bd4293a36e1197dd0209f747bd527a7
Rushank Savant <rushanksavant34@gmail.com>
2022-05-06 15:16:45 +0530

    updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,24 +3,6 @@ require("@nomiclabs/hardhat-waffle")
 const dotenv = require("dotenv");
 dotenv.config();
 
-// module.exports = {
-//   defaultNetwork: "hardhat",
-//   networks: {
-//     hardhat: {
-//       chainId: 1337
-//     },
-
-//   },
-//   solidity: {
-//     version: "0.8.4",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200
-//       }
-//     }
-//   }
-// }
 module.exports = {
   defaultNetwork: "hardhat",
   paths: {
@@ -35,5 +17,5 @@ module.exports = {
       accounts: [process.env.KEY]
     },
   },
-  solidity: "0.8.4",
+  solidity: "0.8.1",
 };
